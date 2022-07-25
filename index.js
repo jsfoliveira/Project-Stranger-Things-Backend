5a05d0a81dfb009196fa92e9ce789c638157c790
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-07-25 18:31:55 -0300

    Requisito 5

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE);
+// const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
